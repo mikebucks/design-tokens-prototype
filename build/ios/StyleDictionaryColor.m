c2e7cdfa2ea4e96aa64b70c7c65664889d001234
Automated Publisher <actions@users.noreply.github.com>
2021-06-24 15:42:44 +0000

    Automated publish: Thu Jun 24 15:42:44 UTC 2021 a0e9a3e0ad8caff4bb4b555e95cf346529b2bdad

--- build/ios/StyleDictionaryColor.m
+++ build/ios/StyleDictionaryColor.m
@@ -3,7 +3,7 @@
 // StyleDictionaryColor.m
 //
 // Do not edit directly
-// Generated on Thu, 24 Jun 2021 15:36:15 GMT
+// Generated on Thu, 24 Jun 2021 15:42:42 GMT
 //
 
 #import "StyleDictionaryColor.h"
