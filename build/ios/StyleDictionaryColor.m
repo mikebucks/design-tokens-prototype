1b9fabb9ab8fd13870ab0788dc9bb19206588cb0
Automated Publisher <actions@users.noreply.github.com>
2021-07-09 19:33:18 +0000

    Automated publish: Fri Jul  9 19:33:18 UTC 2021 f7c8e0b07cfd0a5d164fc80594c89f24da0313b0

--- build/ios/StyleDictionaryColor.m
+++ build/ios/StyleDictionaryColor.m
@@ -3,7 +3,7 @@
 // StyleDictionaryColor.m
 //
 // Do not edit directly
-// Generated on Fri, 09 Jul 2021 19:30:01 GMT
+// Generated on Fri, 09 Jul 2021 19:33:15 GMT
 //
 
 #import "StyleDictionaryColor.h"
--- build/ios/StyleDictionarySize.h
+++ build/ios/StyleDictionarySize.h
@@ -2,7 +2,7 @@
 // StyleDictionarySize.h
 //
 // Do not edit directly
-// Generated on Fri, 09 Jul 2021 19:30:01 GMT
+// Generated on Fri, 09 Jul 2021 19:33:15 GMT
 //
 
 #import <Foundation/Foundation.h>
